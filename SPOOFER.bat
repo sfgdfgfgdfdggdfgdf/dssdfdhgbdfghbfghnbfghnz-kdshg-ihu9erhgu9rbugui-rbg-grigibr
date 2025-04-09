53912d1aace45eead0ec18ee2f24ca529e16f66e
sfgdfgfgdfdggdfgdf <sygood300@gmail.com>
2025-04-09 19:22:40 -0300

    Add files via upload

---
+++ SPOOFER.bat
@@ -0,0 +1,9 @@
+AMIDEWINx64.exe /su
+AMIDEWINx64.exe /bs %random%-%random%-%random%-%random%
+AMIDEWINx64.exe /ss %random%-%random%-%random%-%random%
+AMIDEWINx64.exe /id %random%-%random%-%random%
+AMIDEWINx64.exe /iv %random%-%random%-%random%
+AMIDEWINx64.exe /ivn %random%-%random%-%random%
+AMIDEWINx64.exe /sm %random% TESTING
+AMIDEWINx64.exe /sp %random%
+exit
